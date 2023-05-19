f18442e336275f7fef03ad764203c450e6b047eb
neiljaywarner <njwandroid@gmail.com>
2023-05-19 16:30:27 -0500

    Initial commit from part of 2019 version (main screen) but fixes enough to build and list titles, yay!

---
+++ android/app/src/main/kotlin/org/gordonferguson/gordon_ferguson_teaching_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package org.gordonferguson.gordon_ferguson_teaching_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,165 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'DbProvider.dart';
+import 'Post.dart';
+
+Future<List<Post>> fetchPost() async {
+  final response = await http.get(Uri.parse('http://gordonferguson.org/wp-json/wp/v2/posts?per_page=100'));
+
+  if (response.statusCode == 200) {
+    // If the call to the server was successful, parse the JSON
+    return PostResponse.getPosts(json.decode(response.body));
+  } else {
+    // If that call was not successful, throw an error.
+    //todo: fimber and crashlytics
+    throw Exception('Failed to load posts:${response.statusCode}');
+  }
+}
+
+void main() => runApp(MyApp(posts: fetchPost()));
+
+class MyApp extends StatefulWidget {
+  final Future<List<Post>> posts;
+
+  const MyApp({Key? key, required this.posts}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _selectedIndex = 0;
+  late ArticleFutureBuilder home;
+
+  @override
+  void initState() {
+    super.initState();
+    home = ArticleFutureBuilder(posts: widget.posts);
+  }
+
+  Widget _buildFavoritesWidget(int selectedIndex) {
+    if (selectedIndex==1) {
+      return ArticleFutureBuilder(posts: DBProvider.db.getAllFavorites());
+    } else if (selectedIndex == 2) {
+      //return AboutPage();
+      return Text('TODO: About');
+    } else {
+      return home;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Gordon Ferguson',
+    theme: ThemeData(primarySwatch: Colors.blue),
+    home: Scaffold(
+      appBar: AppBar(title: const Text('Gordon Ferguson')),
+      body: Center(
+        child: _buildFavoritesWidget(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.star), label: 'Favorites'),
+          BottomNavigationBarItem(icon: Icon(Icons.info), label: 'About'),
+        ],
+        currentIndex: _selectedIndex,
+        fixedColor: Theme.of(context).colorScheme.secondary,
+        onTap: _onItemTapped,
+      ),
+    ),
+  );
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+}
+
+class ArticleFutureBuilder extends StatelessWidget {
+  const ArticleFutureBuilder({
+    Key? key,
+    required this.posts,
+  }) : super(key: key);
+
+  final Future<List<Post>> posts;
+
+  @override
+  Widget build(BuildContext context) => FutureBuilder<List<Post>>(
+    future: posts,
+    builder: (context, snapshot) {
+      if (snapshot.connectionState != ConnectionState.done) {
+        debugPrint("Connection state not done");
+        return const CircularProgressIndicator();
+      }
+      debugPrint("Connection state done");
+
+      if (snapshot.hasData && snapshot.data!.isNotEmpty) {
+        debugPrint("has data-data not null: numPosts=${snapshot.data!.length}");
+        return ListView.builder(
+            itemCount: snapshot.data!.length,
+            padding: const EdgeInsets.all(8.0),
+            itemBuilder: (BuildContext context, int i) => PostCard(post: snapshot.data![i])
+        );
+      } else if (snapshot.hasError) {
+        debugPrint("Has Error ${snapshot.error}");
+        return const Center(child:Text('No articles or error loading articles.'));
+      } else {
+        debugPrint("No favorites");
+        return const Center(child:Text('Star an article an it will show up here!'));
+      }
+    },
+  );
+}
+
+class PostCard extends StatelessWidget {
+
+  final Post? post;
+
+  const PostCard({Key? key, this.post}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => InkWell(
+    //onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => DetailScreen(post: post))),
+    child: Card(
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(top:8),
+            child: ListTile(
+              title: Text(post!.title),
+              //subtitle: Html(data: post?.excerpt,),
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
+}
+
+/*
+class AboutPage extends StatelessWidget {
+
+  final resourcesHtml = '''
+  <img src="http://gordonferguson.org/wp-content/uploads/2016/11/Final-Main-Header.jpg"/>
+  <ul>
+    <li><a href='http://gordonferguson.org'>gordonferguson.org</a></li>
+    <li><a href="https://ipibooks.ecwid.com/#!/Gordon-Ferguson/c/18671194/offset=0&sort=nameAsc">Books, videos (ipi)</a></li>
+    <li><a href="mailto:gordonferguson33@gmail.com">Contact</a></li>
+    </a></li>
+    </ul>
+    ''';
+
+  @override
+  Widget build(BuildContext context) => Container(
+    padding: EdgeInsets.all(12),
+    child: Html(data: resourcesHtml, onLinkTap: (String url) => launch(url, forceSafariVC: false),),
+  );
+}
+*/
